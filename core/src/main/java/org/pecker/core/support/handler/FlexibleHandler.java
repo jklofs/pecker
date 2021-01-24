4799de74c8a0d57da3a18e7030c99e1d52192d5b
jklofs <jl.13859385631jl>
2021-01-24 22:21:16 +0800

    init project

--- core/src/main/java/org/pecker/core/support/handler/FlexibleHandler.java
+++ core/src/main/java/org/pecker/core/support/handler/FlexibleHandler.java
@@ -1,4 +1,8 @@
 package org.pecker.core.support.handler;
 
+import com.esotericsoftware.reflectasm.MethodAccess;
+
 public interface FlexibleHandler {
+
+    Object invoke(Object proxy, MethodAccess method, Object[] args);
 }
